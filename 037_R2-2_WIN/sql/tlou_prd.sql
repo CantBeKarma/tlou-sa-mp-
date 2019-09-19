f9b07ee0160f07e6ac4c6d01196c78105fbf4000
Jędrzej <Jedrzej94@users.noreply.github.com>
2019-09-19 23:05:46 +0200

    SQL structure update

--- 037_R2-2_WIN/sql/tlou_prd.sql
+++ 037_R2-2_WIN/sql/tlou_prd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Sep 15, 2019 at 06:54 PM
+-- Generation Time: Sep 19, 2019 at 09:04 PM
 -- Server version: 5.7.26
 -- PHP Version: 7.2.18
 
@@ -41,13 +41,13 @@ CREATE TABLE IF NOT EXISTS `chars_data` (
   `last_ang` float DEFAULT NULL,
   `last_int` tinyint(3) UNSIGNED DEFAULT NULL,
   `last_vw` smallint(5) UNSIGNED DEFAULT NULL,
-  `supplements` tinyint(3) UNSIGNED DEFAULT NULL,
-  `skill_maxhealth` tinyint(3) UNSIGNED DEFAULT NULL,
-  `skill_listenmode` tinyint(3) UNSIGNED DEFAULT NULL,
-  `skill_crafting` tinyint(3) UNSIGNED DEFAULT NULL,
-  `skill_healing` tinyint(3) UNSIGNED DEFAULT NULL,
-  `skill_weaponsway` tinyint(3) UNSIGNED DEFAULT NULL,
-  `skill_shivmaster` tinyint(3) UNSIGNED DEFAULT NULL,
+  `supplements` tinyint(3) UNSIGNED DEFAULT '0',
+  `skill_maxhealth` tinyint(3) UNSIGNED DEFAULT '0',
+  `skill_listenmode` tinyint(3) UNSIGNED DEFAULT '0',
+  `skill_crafting` tinyint(3) UNSIGNED DEFAULT '0',
+  `skill_healing` tinyint(3) UNSIGNED DEFAULT '0',
+  `skill_weaponsway` tinyint(3) UNSIGNED DEFAULT '0',
+  `skill_shivmaster` tinyint(3) UNSIGNED DEFAULT '0',
   PRIMARY KEY (`uid`),
   UNIQUE KEY `guid` (`guid`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
@@ -57,7 +57,7 @@ CREATE TABLE IF NOT EXISTS `chars_data` (
 --
 
 INSERT INTO `chars_data` (`guid`, `uid`, `name`, `health`, `skinid`, `last_x`, `last_y`, `last_z`, `last_ang`, `last_int`, `last_vw`, `supplements`, `skill_maxhealth`, `skill_listenmode`, `skill_crafting`, `skill_healing`, `skill_weaponsway`, `skill_shivmaster`) VALUES
-(1, 1, 'Riddick', 100, 312, 2497.29, -1662.52, 13.3359, 70.1499, 0, 0, 0, 0, 0, 3, 3, 2, 0);
+(1, 1, 'Riddick', 15, 312, 2495.63, -1679.45, 13.339, 346.438, 0, 0, 0, 0, 0, 0, 0, 2, 0);
 
 -- --------------------------------------------------------
 
@@ -86,7 +86,7 @@ CREATE TABLE IF NOT EXISTS `collectibles_data` (
   UNIQUE KEY `uid_3` (`uid`),
   KEY `uid` (`uid`),
   KEY `uid_2` (`uid`)
-) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `collectibles_data`
@@ -100,10 +100,11 @@ INSERT INTO `collectibles_data` (`uid`, `id_item`, `id_type`, `x`, `y`, `z`, `rX
 (18, 0, 0, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'Rain Catcher Note', 'Hey Susan,~n~~n~I just wanted to drop you a quick line and say these rain-catchers were a great idea. Super smart to gather water without leaving the place. I hope you don\'t mind, but I gave the kids a couple of water guns. So of course... I\'ve been drenched all day. If you don\'t want them to have it, let me know and I\'ll take them back. See you at dinner tonight. Fair warning though - Kyle is making his special \"meatloaf\" again. ~n~~n~Ish', 1, 0),
 (19, 0, 0, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'Cornered Note', 'We\'re trapped. I think everyone else is dead. Some of the little ones are with me. I got infected pounding at the door. I don\'t know how long we\'ll hold out. If Ish and the others are alive, maybe they can reach us. They have to reach us. If it comes down to it I\'ll make it quick. ~n~~n~Kyle', 1, 0),
 (20, 0, 0, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'Survivors Note', 'One open door. That\'s all it took. One of us forgot to close a door and a horde of those monsters entered our camp. We shut them in there and wrote a warning on the outside. Susan and a couple of the kids are with me. Every part of my being just wants to give up. It\'d be so easy to surrender to this world. I can\'t do that, though. I have too much faith in humanity. I\'ve seen that we\'re still capable of good. We can make it. I have to stay strong... For her. ~n~~n~Ish', 1, 0),
-(31, 2, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'Firefly Pendant', 'Riddick~n~2019-09-14', 1, 0),
 (32, 0, 0, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'Note from Mom', 'My mom\'s note', 1, 0),
 (33, 0, 0, 2503.19, -1686.33, 12.5409, 0, 0, 0, 0, 0, 'Do_not_enter', 'Seems_like_we_did_it_Do_not_open_the_garage', 1, 0),
-(34, 1, 0, 2499.31, -1675.99, 12.3498, 0, 0, 0, 0, 0, 'MyTestTitle', 'MyTestMsg', 1, 0);
+(34, 1, 0, 2499.31, -1675.99, 12.3498, 0, 0, 0, 0, 0, 'MyTestTitle', 'MyTestMsg', 1, 0),
+(37, 2, 1, 2487.43, -1659.7, 12.3438, 120, 0, 0, 0, 0, 'Firefly Pendant', 'Riddick~n~2019-09-16', 1, 0),
+(38, 2, 1, 2475.03, -1665.76, 12.3195, 120, 0, 0, 0, 0, 'Firefly Pendant', 'Riddick~n~2019-09-16', 1, 0);
 
 -- --------------------------------------------------------
 
@@ -123,7 +124,7 @@ CREATE TABLE IF NOT EXISTS `items_data` (
   `rZ` float DEFAULT NULL,
   `virtualWorld` smallint(5) UNSIGNED DEFAULT NULL,
   `interiorId` tinyint(3) UNSIGNED DEFAULT NULL,
-  `amount` tinyint(3) UNSIGNED NOT NULL DEFAULT '0',
+  `amount` smallint(5) UNSIGNED NOT NULL DEFAULT '0',
   `meleeExtCnd` smallint(6) UNSIGNED DEFAULT NULL,
   `uid_owner` smallint(6) NOT NULL DEFAULT '-1',
   `is_created` tinyint(1) UNSIGNED NOT NULL DEFAULT '1',
@@ -138,25 +139,26 @@ CREATE TABLE IF NOT EXISTS `items_data` (
 
 DROP TABLE IF EXISTS `player_data`;
 CREATE TABLE IF NOT EXISTS `player_data` (
-  `guid` smallint(5) UNSIGNED NOT NULL,
+  `guid` smallint(5) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` varchar(24) NOT NULL,
   `ip_address` varchar(24) DEFAULT NULL,
   `register_date` datetime DEFAULT NULL,
   `last_login_date` datetime DEFAULT NULL,
   `password` varchar(129) NOT NULL,
-  `admin_level` tinyint(3) UNSIGNED DEFAULT NULL,
-  `volume_level` tinyint(3) UNSIGNED DEFAULT NULL,
+  `admin_level` tinyint(3) UNSIGNED DEFAULT '0',
+  `volume_level` tinyint(3) UNSIGNED DEFAULT '100',
   `ambient_toggle` tinyint(3) UNSIGNED DEFAULT NULL,
   `status` tinyint(3) UNSIGNED DEFAULT NULL,
   PRIMARY KEY (`guid`),
   KEY `volume_level` (`volume_level`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `player_data`
 --
 
-INSERT INTO `player_data` (`guid`, `ip_address`, `register_date`, `last_login_date`, `password`, `admin_level`, `volume_level`, `ambient_toggle`, `status`) VALUES
-(1, NULL, '2014-12-24 00:00:00', '2019-09-15 20:50:49', 'C0FB3E94DABA675E3FD32E8C1C018D2A6435CA5145738AE86429CA5D3AA14069', 4, NULL, NULL, NULL);
+INSERT INTO `player_data` (`guid`, `name`, `ip_address`, `register_date`, `last_login_date`, `password`, `admin_level`, `volume_level`, `ambient_toggle`, `status`) VALUES
+(1, 'Riddick', '127.0.0.1', '2019-09-19 20:14:58', '2019-09-19 22:54:20', '$2y$12$RVTYSBeuUFLJXR/qcTTqLek2BNq.ykaYQrKBjoYGxrEkCFHl5gaze', 0, 100, NULL, NULL);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
